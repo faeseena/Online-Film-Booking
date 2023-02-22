eabd57b19c69590f39605ad9f219f093e639305c
hp <hp@DESKTOP-3BS2DH0>
2023-02-22 23:17:10 +0530

    This is my Online Film Booking Application

---
+++ MovieBooking/src/main/java/com/example/demo/BusConfig.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import com.example.demo.filter.BusFilter;
+
+@Configuration
+public class BusConfig implements WebMvcConfigurer {
+	
+	@Bean
+	public BusFilter getFilter() {
+		return new BusFilter();
+	}
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		
+		registry.addInterceptor(getFilter()).addPathPatterns("/*");
+	}
+
+}
